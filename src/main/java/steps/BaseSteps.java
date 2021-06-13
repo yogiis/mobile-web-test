bffd5b7f66c96a616f5ba2830ab71cc7f1ef0338
Yogi Is Ariyanto <yogi@qasir.id>
2021-06-13 22:23:28 +0700

    add gitignore , update pom mvn, remove screenshot

--- src/main/java/steps/BaseSteps.java
+++ src/main/java/steps/BaseSteps.java
@@ -3,8 +3,6 @@ package steps;
 import cucumber.api.Scenario;
 import driver.mobile.ThreadMobileDriver;
 import driver.web.ThreadWebDriver;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.Objects;
@@ -24,13 +22,10 @@ public class BaseSteps {
     protected void setupCucumberWeb() {
         System.out.println("Cucumber Base Step");
         wait = new WebDriverWait(ThreadWebDriver.getTLDriver(), 10);
-
     }
 
     protected void tearDownMobile(Scenario scenario) {
         if (scenario.isFailed()) {
-            final byte[] screenshot = ((TakesScreenshot) ThreadMobileDriver.getTldriver()).getScreenshotAs(OutputType.BYTES);
-            scenario.embed(screenshot, "image/png");
             try {
                 if (ThreadMobileDriver.getTldriver() != null)
                     ThreadMobileDriver.getTldriver().quit();
@@ -43,8 +38,6 @@ public class BaseSteps {
 
     protected void tearDownWeb(Scenario scenario) {
         if (scenario.isFailed()) {
-            final byte[] screenshot = ((TakesScreenshot) ThreadWebDriver.getTLDriver()).getScreenshotAs(OutputType.BYTES);
-            scenario.embed(screenshot, "image/png");
             try {
                 if (ThreadWebDriver.getTLDriver() != null)
                     ThreadWebDriver.getTLDriver().quit();
